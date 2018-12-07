4bcfa5037840220e80ca9f72dd7c334446d95990
matengya <matengya@19lou.com>
2018-12-07 14:06:52 +0800

    通讯录 列表

--- configuration/ToolMacro.h
+++ configuration/ToolMacro.h
@@ -17,6 +17,7 @@
 #define  HC_statusBarHeight [[UIApplication sharedApplication] statusBarFrame].size.height
 #define  HC_safeBottom     [[UIApplication sharedApplication] statusBarFrame].size.height == 44 ? 34 : 0
 #define  HC_naviHeight     [[UIApplication sharedApplication] statusBarFrame].size.height + 44
+#define  HC_tabBarHeight   (HC_iPhoneX ? (49.f+34.f) : 49.f)
 #define  HC_750Ratio       HC_windowWidth / 750.0
 #define  HC_750Font(x)     [UIFont systemFontOfSize:x / HC_750Ratio]
 
--- xiaoZhang/ addressBook/vc/AdressBookVC.m
+++ xiaoZhang/ addressBook/vc/AdressBookVC.m
@@ -11,31 +11,13 @@
 #import "ApplyPermission.h"
 #import "AddressBookCell.h"
 
-#define FULL_WIDTH  [UIScreen mainScreen].bounds.size.width
-#define FULL_HEIGHT [UIScreen mainScreen].bounds.size.height
-
-#define isIOS9 ([[[UIDevice currentDevice] systemVersion] floatValue] >= 9.0)
-
-//屏幕比例 屏宽／375
-#define SCREEN_SCALE (FUll_VIEW_WIDTH/375)
-
-//iPHONE_X 判断
-#define IS_iPHONE_X ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
-// 状态栏高度
-#define STATUS_BAR_HEIGHT (IS_iPHONE_X ? 44.f : 20.f)
-// 导航栏高度
-#define NAVIGATION_BAR_HEIGHT (IS_iPHONE_X ? 88.f : 64.f)
-// tabBar高度
-#define TAB_BAR_HEIGHT (IS_iPHONE_X ? (49.f+34.f) : 49.f)
-// home indicator
-#define HOME_INDICATOR_HEIGHT (IS_iPHONE_X ? 34.f : 0.f)
-
 
 @interface AdressBookVC ()<UITableViewDelegate,UITableViewDataSource> {
     UITableView *_myTableView;
     NSArray *_alphabetArray;
     NSMutableArray *_addressBookArray;
 }
+
 
 @end
 
@@ -50,13 +32,15 @@
     [self setUI];
     
     [[ApplyPermission sharedInstance] applyPermissionWithPermissionType:ApplyPermissionTypeAddressBook complete:^(BOOL isAllow, BOOL isFirst) {
-        NSArray * a =  [AddressBookUtils getAllPeopleInfoToModel];
-        NSLog(@"%@", a);
+        NSArray *array =  [AddressBookUtils getAllPeopleInfoToModel];
+        NSLog(@"-------%@", array);
+        _addressBookArray = array;
     }];
+
 }
 
 - (void)setUI {
-    _myTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, NAVIGATION_BAR_HEIGHT, FULL_WIDTH, FULL_HEIGHT-NAVIGATION_BAR_HEIGHT-TAB_BAR_HEIGHT) style:UITableViewStylePlain];
+    _myTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, HC_naviHeight, HC_windowWidth, HC_windowHeight-HC_tabBarHeight-HC_naviHeight) style:UITableViewStylePlain];
     [_myTableView registerClass:[AddressBookCell class] forCellReuseIdentifier:@"myCellId"];
     _myTableView.dataSource = self;
     _myTableView.delegate = self;
@@ -77,8 +61,12 @@
     if(!cell) {
         cell = [[AddressBookCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"myCellId"];
     }
+//    _addressBookArray
+    
+    HCContact *model = [[HCContact alloc] init];
+    model = _addressBookArray[indexPath.row];
     cell.iconView.image = [UIImage imageNamed:@"header"];
-    cell.nameLabel.text = @"大大大大王叫我来巡山";
+    cell.nameLabel.text = model.name;
     return cell;
 }
 
@@ -87,7 +75,7 @@
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, FULL_WIDTH, 30)];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, HC_windowWidth, 30)];
     label.backgroundColor = [UIColor lightGrayColor];
     label.text = [NSString stringWithFormat:@"%@",_alphabetArray[section]];
     return label;
