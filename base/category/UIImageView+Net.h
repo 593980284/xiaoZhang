a47488bc4db54adaef6e2e6c2435d35811392ee6
胡超_iOS <huchao@meeruu.com>
2018-12-09 18:41:11 +0800

    添加注释

--- base/category/UIImageView+Net.h
+++ base/category/UIImageView+Net.h
@@ -9,7 +9,17 @@
 #import <UIKit/UIKit.h>
 
 @interface UIImageView (Net)
+
+/**
+ 1.如果imageString没有http，会拼接默认域名
+ 2.自带有默认图片
+ */
 - (void)setImageWithImageString:(NSString *)imageString;
+
+/**
+1.如果没有http，会拼接默认域名
+2.默认图片需要自己设置
+*/
 - (void)setImageWithImageString:(NSString *)imageString placeholderString:(NSString *)placeholderString;
 - (void)setImageWithImageString:(NSString *)imageString placeholder:(UIImage *)placeholder;
 @end
